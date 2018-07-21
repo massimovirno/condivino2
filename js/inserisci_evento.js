f63ac9126e0cde523018c049b83bdfddadb7475b
Massimo Virno <io@massimovirno.it>
2018-07-21 20:13:10 +0200

    Pulizia js

--- js/inserisci_evento.js
+++ js/inserisci_evento.js
@@ -1,39 +1,4 @@
 $(document).ready(function(){
-    var acList = ["Agrigento", "Alessandria", "Ancona", "Aosta", "Arezzo","Ascoli Piceno","Asti","Avellino","Bari,Barletta-Andria-Trani","Belluno","Benevento","Bergamo","Biella","Bologna","Bolzano","Brescia","Brindisi","Cagliari","Caltanissetta","Campobasso","Carbonia-Iglesias","Caserta","Catania","Catanzaro","Chieti","Como","Cosenza","Cremona","Crotone","Cuneo","Enna","Fermo","Ferrara","Firenze","Foggia","Forli-Cesena","Frosinone","Genova","Gorizia","Grosseto","Imperia","Isernia","La Spezia","L'Aquila","Latina","Lecce","Lecco","Livorno","Lodi","Lucca","Macerata","Mantova","Massa-Carrara","Matera","Messina","Milano","Modena","Monza e della Brianza","Napoli","Novara","Nuoro","Olbia-Tempio","Oristano","Padova","Palermo","Parma","Pavia","Perugia","Pesaro e Urbino","Pescara","Piacenza","Pisa","Pistoia","Pordenone","Potenza","Prato","Ragusa","Ravenna","Reggio Calabria","Reggio Emilia","Rieti","Rimini","Roma","Rovigo","Salerno","Medio Campidano","Sassari","Savona","Siena","Siracusa","Sondrio","Taranto","Teramo","Terni","Torino","Trapani","Trento","Treviso","Trieste", "Udine", "Varese","Venezia","Verbano-Cusio-Ossola","Vercelli","Verona","Vibo Valentia","Vicenza","Viterbo"];
-        $('#citta_partenza').autocomplete({
-                 minLength: 2,
-                 source: function( request, response ) {
-                            var matches = $.map( acList, function(acItem) {
-                            if ( acItem.toUpperCase().indexOf(request.term.toUpperCase()) === 0 ) {
-                                return acItem;
-                            }
-                            });
-                          response(matches);
-                          },
-                 change: function (event, ui) {
-                            if(!ui.item){
-                                $("#citta_partenza").val("");
-                            }
-                         }                 
-        });
-        
-    var acList = ["Agrigento", "Alessandria", "Ancona", "Aosta", "Arezzo","Ascoli Piceno","Asti","Avellino","Bari,Barletta-Andria-Trani","Belluno","Benevento","Bergamo","Biella","Bologna","Bolzano","Brescia","Brindisi","Cagliari","Caltanissetta","Campobasso","Carbonia-Iglesias","Caserta","Catania","Catanzaro","Chieti","Como","Cosenza","Cremona","Crotone","Cuneo","Enna","Fermo","Ferrara","Firenze","Foggia","Forli-Cesena","Frosinone","Genova","Gorizia","Grosseto","Imperia","Isernia","La Spezia","L'Aquila","Latina","Lecce","Lecco","Livorno","Lodi","Lucca","Macerata","Mantova","Massa-Carrara","Matera","Messina","Milano","Modena","Monza e della Brianza","Napoli","Novara","Nuoro","Olbia-Tempio","Oristano","Padova","Palermo","Parma","Pavia","Perugia","Pesaro e Urbino","Pescara","Piacenza","Pisa","Pistoia","Pordenone","Potenza","Prato","Ragusa","Ravenna","Reggio Calabria","Reggio Emilia","Rieti","Rimini","Roma","Rovigo","Salerno","Medio Campidano","Sassari","Savona","Siena","Siracusa","Sondrio","Taranto","Teramo","Terni","Torino","Trapani","Trento","Treviso","Trieste", "Udine", "Varese","Venezia","Verbano-Cusio-Ossola","Vercelli","Verona","Vibo Valentia","Vicenza","Viterbo"];
-        $('#citta_arrivo').autocomplete({
-                 minLength: 2,
-                 source: function( request, response ) {
-                            var matches = $.map( acList, function(acItem) {
-                            if ( acItem.toUpperCase().indexOf(request.term.toUpperCase()) === 0 ) {
-                                return acItem;
-                            }
-                            });
-                          response(matches);
-                          },
-                 change: function (event, ui) {
-                            if(!ui.item){
-                                $("#citta_arrivo").val("");
-                            }
-                         }                 
-        });
     
     $('#inserimento').validate({
         rules: {
