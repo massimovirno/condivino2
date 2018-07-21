1527d7c563d8087f795c8dfbf8837360c7afac39
Massimo Virno <io@massimovirno.it>
2018-07-21 13:19:01 +0200

    Pulizia javascript

--- js/ricerca_amministrazione_eventi.js
+++ js/ricerca_amministrazione_eventi.js
@@ -1,41 +1,7 @@
 $(document).ready(function(){
-    var acList = ["Agrigento", "Alessandria", "Ancona", "Aosta", "Arezzo","Ascoli Piceno","Asti","Avellino","Bari,Barletta-Andria-Trani","Belluno","Benevento","Bergamo","Biella","Bologna","Bolzano","Brescia","Brindisi","Cagliari","Caltanissetta","Campobasso","Carbonia-Iglesias","Caserta","Catania","Catanzaro","Chieti","Como","Cosenza","Cremona","Crotone","Cuneo","Enna","Fermo","Ferrara","Firenze","Foggia","Forli-Cesena","Frosinone","Genova","Gorizia","Grosseto","Imperia","Isernia","La Spezia","L'Aquila","Latina","Lecce","Lecco","Livorno","Lodi","Lucca","Macerata","Mantova","Massa-Carrara","Matera","Messina","Milano","Modena","Monza e della Brianza","Napoli","Novara","Nuoro","Olbia-Tempio","Oristano","Padova","Palermo","Parma","Pavia","Perugia","Pesaro e Urbino","Pescara","Piacenza","Pisa","Pistoia","Pordenone","Potenza","Prato","Ragusa","Ravenna","Reggio Calabria","Reggio Emilia","Rieti","Rimini","Roma","Rovigo","Salerno","Medio Campidano","Sassari","Savona","Siena","Siracusa","Sondrio","Taranto","Teramo","Terni","Torino","Trapani","Trento","Treviso","Trieste", "Udine", "Varese","Venezia","Verbano-Cusio-Ossola","Vercelli","Verona","Vibo Valentia","Vicenza","Viterbo"];
-        $('#citta_partenza_ricerca').autocomplete({
-                 minLength: 2,
-                 source: function( request, response ) {
-                            var matches = $.map( acList, function(acItem) {
-                            if ( acItem.toUpperCase().indexOf(request.term.toUpperCase()) === 0 ) {
-                                return acItem;
-                            }
-                            });
-                          response(matches);
-                          },
-                 change: function (event, ui) {
-                            if(!ui.item){
-                                $("#citta_partenza_ricerca").val("");
-                            }
-                         }                 
-        });
-    
-    var acList = ["Agrigento", "Alessandria", "Ancona", "Aosta", "Arezzo","Ascoli Piceno","Asti","Avellino","Bari,Barletta-Andria-Trani","Belluno","Benevento","Bergamo","Biella","Bologna","Bolzano","Brescia","Brindisi","Cagliari","Caltanissetta","Campobasso","Carbonia-Iglesias","Caserta","Catania","Catanzaro","Chieti","Como","Cosenza","Cremona","Crotone","Cuneo","Enna","Fermo","Ferrara","Firenze","Foggia","Forli-Cesena","Frosinone","Genova","Gorizia","Grosseto","Imperia","Isernia","La Spezia","L'Aquila","Latina","Lecce","Lecco","Livorno","Lodi","Lucca","Macerata","Mantova","Massa-Carrara","Matera","Messina","Milano","Modena","Monza e della Brianza","Napoli","Novara","Nuoro","Olbia-Tempio","Oristano","Padova","Palermo","Parma","Pavia","Perugia","Pesaro e Urbino","Pescara","Piacenza","Pisa","Pistoia","Pordenone","Potenza","Prato","Ragusa","Ravenna","Reggio Calabria","Reggio Emilia","Rieti","Rimini","Roma","Rovigo","Salerno","Medio Campidano","Sassari","Savona","Siena","Siracusa","Sondrio","Taranto","Teramo","Terni","Torino","Trapani","Trento","Treviso","Trieste", "Udine", "Varese","Venezia","Verbano-Cusio-Ossola","Vercelli","Verona","Vibo Valentia","Vicenza","Viterbo"];
-        $('#citta_arrivo_ricerca').autocomplete({
-                 minLength: 2,
-                 source: function( request, response ) {
-                            var matches = $.map( acList, function(acItem) {
-                            if ( acItem.toUpperCase().indexOf(request.term.toUpperCase()) === 0 ) {
-                                return acItem;
-                            }
-                            });
-                          response(matches);
-                          },
-                 change: function (event, ui) {
-                            if(!ui.item){
-                                $("#citta_arrivo_ricerca").val("");
-                            }
-                         }                 
-        });
+   
 // Inserisco il calendario di jQuery UI
-    $("#data_partenza_ricerca").datepicker({
+$("#data_evento_ricerca").datepicker({
         showOn: "button",
         buttonImage: "templates/main/template/img/calendario.gif",
         buttonImageOnly: true,
@@ -45,7 +11,7 @@ $(document).ready(function(){
         dateFormat: "yy/mm/dd",
         defaultDate: "2000/01/01"
     });
-    
+
 $('.mostra_avanzata').on("click",function(){
         $.ajax({
             url:'index.php',
@@ -69,7 +35,7 @@ $('#submit_ricerca_eventi').on("click",function(){
             url:'index.php',
             dataType:'html',
             type:'GET',
-            data:{controller:'ricerca', task:'ricerca_eventi', ricerca:true, citta_partenza_ricerca:$('#citta_partenza_ricerca').val(), citta_arrivo_ricerca:$('#citta_arrivo_ricerca').val(), data_partenza_ricerca:$('#data_partenza_ricerca').val()},
+            data:{controller:'ricerca', task:'ricerca_eventi', ricerca:true, nome_evento_ricerca:$('#nome_evento_ricerca').val(), vino_evento_ricerca:$('#vino_evento_ricerca').val(), data_evento_ricerca:$('#data_evento_ricerca').val()},
             success:ricerca_eventi
         });    
     });
